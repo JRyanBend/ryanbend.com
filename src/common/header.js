0c4c9d5f9c0120a87915bcb0501d503b9fd1c5d8
Trevellyan <rbend@shaw.ca>
2017-07-05 20:55:06 -0500

    Removed old code

--- src/common/header.js
+++ src/common/header.js
@@ -2,32 +2,7 @@ import React from "react";
 
 export default React.createClass({
 
-	componentDidMount: function() {
-    	const button_projects = document.getElementById("button_projects"),
-    		  button_blog = document.getElementById("button_blog");
-
-    	let projects = document.getElementById("projects"),
-    		blog = document.getElementById("blog");
-
-
-    	button_projects.addEventListener("click", function(event) {
-    		projects.className = "page page--show";
-    		blog.className = "page";
-    	})
-
-    	button_blog.addEventListener("click", function(event) {
-    		blog.className = "page page--show"
-    		projects.className = "page";
-    	})
-  	},
-
     render: function() {
-
-    	
-    	console.log("Hello");
-
-    	console.log(document.getElementById("button_projects"))
-
         return (
             <div className="header">
 	            <div className="header__hero">
